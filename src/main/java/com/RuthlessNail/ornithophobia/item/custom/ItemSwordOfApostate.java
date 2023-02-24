05371334c783e4d734b2941d75ba3e00a22b31f5
RuthlessNail <ruthlessnail@gmail.com>
2023-02-24 20:39:02 +1100

    made Sword of Apostate able to disable shield

--- src/main/java/com/RuthlessNail/ornithophobia/item/custom/ItemSwordOfApostate.java
+++ src/main/java/com/RuthlessNail/ornithophobia/item/custom/ItemSwordOfApostate.java
@@ -1,12 +1,21 @@
 package com.RuthlessNail.ornithophobia.item.custom;
 
-import net.minecraft.world.item.AxeItem;
-import net.minecraft.world.item.SwordItem;
-import net.minecraft.world.item.Tier;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.*;
 
 public class ItemSwordOfApostate extends SwordItem {
     public ItemSwordOfApostate(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
         super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
 
+    }
+
+    @Override
+    public Rarity getRarity(ItemStack pStack) {
+        return Rarity.EPIC;
+    }
+
+    @Override
+    public boolean canDisableShield(ItemStack stack, ItemStack shield, LivingEntity entity, LivingEntity attacker) {
+        return true;
     }
 }
